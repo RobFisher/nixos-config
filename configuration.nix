83c575847513435afae17eb6e367b88c05ff3369
Rob Fisher <robfisher@gmail.com>
2023-12-22 23:31:42 +0000

    Support for mounting Samba shares.

--- configuration.nix
+++ configuration.nix
@@ -143,6 +143,7 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
     wezterm
+    cifs-utils
     nvd
     nushellFull
     nushellPlugins.formats
