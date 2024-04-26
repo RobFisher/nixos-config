413013e1b7460d3a7c60b7ab9782145c5d4f1152
Rob Fisher <robfisher@gmail.com>
2024-04-26 09:02:45 +0100

    Renamed sddm option.

--- configuration.nix
+++ configuration.nix
@@ -139,7 +139,7 @@ in
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
+  services.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
